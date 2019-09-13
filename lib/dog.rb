5633b6b5812b7219ef72905368f880f47574a5d6
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-09-13 18:45:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Add your code here
+class Dog 
+  attr_accessor :name 
+  
+  @@all = [] 
+  
+  def initialize(name) 
+    @name = name 
+    @@all << self 
+  end 
+  
+ def @@all.all 
+  @@all 
+end 
+
+end 
